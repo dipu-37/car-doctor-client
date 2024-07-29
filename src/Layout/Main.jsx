11d02acc5b356926c43c4552700b57467e2930e0
dipu-37 <razibul1105nstu@gmail.com>
2024-07-29 19:07:24 +0700

    router and header footer set up

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+
+import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
+import NavBar from '../Shared/NavBar/NavBar';
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+            
+
+        </div>
+    );
+};
+
+export default Main;
